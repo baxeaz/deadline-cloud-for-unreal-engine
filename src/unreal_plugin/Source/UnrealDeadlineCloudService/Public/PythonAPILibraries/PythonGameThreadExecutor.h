75b15020d5d5001e2f92806b9825f8def45196bb
Brian Axelson <86568017+baxeaz@users.noreply.github.com>
2024-10-30 23:40:16 +0000

    Adding TickableEditorObject.h include

--- src/unreal_plugin/Source/UnrealDeadlineCloudService/Public/PythonAPILibraries/PythonGameThreadExecutor.h
+++ src/unreal_plugin/Source/UnrealDeadlineCloudService/Public/PythonAPILibraries/PythonGameThreadExecutor.h
@@ -3,29 +3,32 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Editor/UnrealEd/Public/TickableEditorObject.h"
 #include "PythonAPILibraries/PythonAPILibrary.h"
-#include "UObject/Object.h"
 #include "PythonGameThreadExecutor.generated.h"
+#include "UObject/Object.h"
 
 /**
- * Python Game thread executor. Intended to execute python code in main thread in Unreal on Editor tick.
- * See implementation: OnTickThreadExecutorImplementation
- * in deadline-cloud-for-unreal: src/deadline/unreal_adaptor/UnrealClient/unreal_client.py
+ * Python Game thread executor. Intended to execute python code in main thread
+ * in Unreal on Editor tick. See implementation:
+ * OnTickThreadExecutorImplementation in deadline-cloud-for-unreal:
+ * src/deadline/unreal_adaptor/UnrealClient/unreal_client.py
  */
 UCLASS(Blueprintable)
-class UNREALDEADLINECLOUDSERVICE_API UPythonGameThreadExecutor : public UObject, public TPythonAPILibraryBase<UPythonGameThreadExecutor>, public FTickableEditorObject
-{
-	GENERATED_BODY()
+class UNREALDEADLINECLOUDSERVICE_API UPythonGameThreadExecutor
+    : public UObject,
+      public TPythonAPILibraryBase<UPythonGameThreadExecutor>,
+      public FTickableEditorObject {
+  GENERATED_BODY()
 public:
-
-	/** This method is called in Tick method. This way the code it runs will be executed in Editor Main thread */
-	UFUNCTION(BlueprintImplementableEvent, Category = Python)
-	void Execute(float DeltaTime) const;
+  /** This method is called in Tick method. This way the code it runs will be
+   * executed in Editor Main thread */
+  UFUNCTION(BlueprintImplementableEvent, Category = Python)
+  void Execute(float DeltaTime) const;
 
 private:
-
-	/** FTickableEditorObject interface */
-	virtual void Tick(float DeltaTime) override;
-	virtual bool IsTickable() const override;
-	virtual TStatId GetStatId() const override;
+  /** FTickableEditorObject interface */
+  virtual void Tick(float DeltaTime) override;
+  virtual bool IsTickable() const override;
+  virtual TStatId GetStatId() const override;
 };
